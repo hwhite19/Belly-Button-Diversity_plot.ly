9a4bbbb6571553c3311cb7418bbe28861e988877
hwhite19 <henry.d.white@gmail.com>
2022-02-02 11:22:15 -0500

    \

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,3 +1,25 @@
-let data = d3.json("samples.json");
 
-console.log(data);
+
+// function that updates the dashboard
+
+// function that populates the meta data
+
+// function that builds the graphs
+
+// function that initializes the dashboard
+function initialize()
+{
+
+
+    // access the dropdown selector from the index.html file
+    var select = d3.select("#selDataset");
+
+    // use d3.json inorder to get the data
+    d3.json("samples.json").then((data) => {
+        let sampleNames = data.names;
+        console.log(sampleNames)
+    });
+}
+
+// call the intialize function
+initialize();
